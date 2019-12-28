9a63aa6e33338b564b24e5e0e7df2c887151903f
Mayur <bhatia.ma@husky.neu.edu>
2019-12-28 16:37:20 -0500

    Add fade animation to nav, Add jello animation to brand logo

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeEarly } from 'src/app/animate';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  animations: [
+    fadeEarly
+  ]
 })
+
 export class NavComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    const element = document.querySelector('.navbar-brand');
+    element.addEventListener('mouseenter', function() {
+      this.classList.add('animated', 'jello', 'ease-in');
+    });
+    element.addEventListener('mouseleave', function() {
+      this.classList.remove('animated', 'jello', 'ease-in');
+    });
   }
 
 }
