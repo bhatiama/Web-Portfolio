94bcc504e608866b35809675719192804c6f82c6
Mayur <bhatia.ma@husky.neu.edu>
2020-02-03 23:32:26 -0500

    Resolve navigation active link issue

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -18,22 +18,40 @@ export class NavComponent implements OnInit {
 
   ngOnInit() {
     const element = document.querySelector('.navbar-brand');
-    element.addEventListener('mouseenter', function() {
+    element.addEventListener('mouseenter', function () {
       this.classList.add('animated', 'jello', 'ease-in');
     });
-    element.addEventListener('mouseleave', function() {
+    element.addEventListener('mouseleave', function () {
       this.classList.remove('animated', 'jello', 'ease-in');
     });
 
     const nav = document.querySelectorAll('.nav-item');
-    for(let i=0; i<nav.length; i++){
-        nav[i].addEventListener('click', function() {
-          const act = document.querySelectorAll('.active');
-          for(let j = 0; j<act.length; j++ ){
-            act[j].classList.remove('active');
-          }
-          this.classList.add('active');
-        });
+    const path = location.pathname.split('/');
+
+    const act = document.querySelectorAll('.active');
+    // tslint:disable-next-line: prefer-for-of
+    for (let j = 0; j < act.length; j++) {
+      act[j].classList.remove('active');
+    }
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < nav.length; i++) {
+      if (nav[i].classList.contains(path[1])) {
+        nav[i].classList.add('active');
+      }
+    }
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < nav.length; i++) {
+      nav[i].addEventListener('click', function () {
+        const active = document.querySelectorAll('.active');
+        // tslint:disable-next-line: prefer-for-of
+        for (let j = 0; j < active.length; j++) {
+          active[j].classList.remove('active');
+        }
+        this.classList.add('active');
+      });
+      console.log(location.pathname.split('/'));
     }
   }
 
